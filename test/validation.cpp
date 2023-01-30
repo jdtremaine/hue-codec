ff9677c819e4196a2d63a6e334a47ab538646130
Jamie Tremaine <jamie.tremaine@gmail.com>
2023-01-30 16:30:52 -0500

    Refactored RealSense encoding as RSEncoder class
    RSEncoder wraps RealSense software device setup and hue encoding.
    Note that RSEncoder does allow for inversion.

--- test/validation.cpp
+++ test/validation.cpp
@@ -3,17 +3,112 @@
 #include <hue_codec.h>							// The header-only hue codec
 #include <librealsense2/rs.hpp>					// Include RealSense Cross Platform API
 #include <librealsense2/hpp/rs_internal.hpp>	// RealSense software-only device
-#include "ref/rs-colorize.h"					// Corrected reference decoder 
+#include "ref/rs-colorize.h"					// Corrected reference decoder
+#include <memory>								// shared_ptr
 #include <fmt/core.h>							// formatted terminal output
 #include <fmt/color.h>							// formatted ANSI terminal output
 #include "common.h"								// common code
 
+class RSEncoder
+{	// A wrapper around the RealSense encoder using a software-only device
+	public:
+	RSEncoder(int height, int width, float depth_min_m, float depth_max_m, float depth_scale)
+	: m_height(height)
+	, m_width(width)
+	, m_depth_scale(depth_scale)
+	, m_frame_number(1)
+	{
+		// Initialize the RealSense-native colorizer to Hue colorization
+		//m_color_map.set_option(RS2_OPTION_HISTOGRAM_EQUALIZATION_ENABLED, 0);
+		m_color_map.set_option(RS2_OPTION_HISTOGRAM_EQUALIZATION_ENABLED, 0);
+		m_color_map.set_option(RS2_OPTION_COLOR_SCHEME, 9.0f);		// Hue colorization
+		m_color_map.set_option(RS2_OPTION_MAX_DISTANCE, depth_max_m);
+		m_color_map.set_option(RS2_OPTION_MIN_DISTANCE, depth_min_m);
 
-TEST_CASE("test value encoder against corrected reference decoder") 
+		auto depth_sensor = m_dev.add_sensor("Depth"); // Define a single depth sensor
+		mp_depth_sensor = std::make_unique<rs2::software_sensor>(depth_sensor);
+
+		rs2_intrinsics depth_intrinsics = {
+			m_width, m_height,
+			(float)m_width / 2, (float)m_height / 2,
+			(float)m_width, (float)m_height,
+			RS2_DISTORTION_BROWN_CONRADY, { 0,0,0,0,0 }
+		};
+
+		m_depth_stream = mp_depth_sensor->add_video_stream({
+			RS2_STREAM_DEPTH, 0, 0,
+			m_width, m_height, 60, 2,
+			RS2_FORMAT_Z16, depth_intrinsics
+		});
+
+		// Compare all streams according to timestamp
+		m_dev.create_matcher( RS2_MATCHER_DEFAULT );
+		mp_depth_sensor->open(m_depth_stream);
+		mp_depth_sensor->start(m_sync);
+	}
+
+
+	void encode(const cv::Mat& src, cv::Mat& dst)
+	{
+		rs2_time_t timestamp = (rs2_time_t)m_frame_number * 16;
+		auto domain = RS2_TIMESTAMP_DOMAIN_HARDWARE_CLOCK;
+
+		// Set synthetic depth frame data and parameters
+		mp_depth_sensor->on_video_frame({
+			 (uint16_t*)src.data,		// Frame pixels from capture API
+			 []( void * ) {},           // Custom deleter (if required)
+			 m_width * 2,				// Stride
+			 2,
+			 timestamp, domain, m_frame_number,
+			 m_depth_stream,
+			 m_depth_scale
+		});
+
+		rs2::frameset fset = m_sync.wait_for_frames();
+		m_frame_depth = fset.first_or_default(RS2_STREAM_DEPTH);
+
+		// Use RealSense colorizer to hue-encode depth data
+		rs2::frame m_frame_color = m_frame_depth.apply_filter(m_color_map);
+
+		// Create OpenCV matrix from the colorized depth data
+		dst = cv::Mat(m_height, m_width, CV_8UC3, (void*)m_frame_color.get_data());
+
+		// Convert RealSense RGB colorized depth to OpenCV-standard BGR.
+		cv::cvtColor(dst, dst, cv::COLOR_RGB2BGR);
+
+		// Increment the frame number
+		m_frame_number++;
+	}
+
+	cv::Mat encode(const cv::Mat& src)
+	{
+		cv::Mat dst;
+		encode(src, dst);
+		return dst;
+	}
+
+
+	private:
+	rs2::colorizer m_color_map;
+	rs2::software_device m_dev;				// software-only device
+	std::unique_ptr<rs2::software_sensor> mp_depth_sensor;	// software-only sensor
+	rs2::frame m_frame_depth, m_frame_color;
+	rs2::syncer m_sync;
+	rs2::stream_profile m_depth_stream;
+	int m_height, m_width;
+	float m_depth_scale;
+	bool m_inverted;
+	int m_frame_number;
+};
+
+
+
+
+TEST_CASE("test value encoder against corrected reference decoder")
 {	// The source for the reference decoder is the development branch of rs-colorize
 	// Compare ref/rs-colorize.cpp to the original to see the changes
 
-	for (uint16_t value=0; value<1530; value++)
+	for (uint16_t value=0; value<HUE_ENCODER_MAX; value++)
 	{
 		cv::Vec3b bgr = hue_encode_value(value);
 		uint16_t ref_decoded_value = RGBtoD(bgr[2], bgr[1], bgr[0]);
@@ -28,82 +123,25 @@ TEST_CASE("test RealSense encoder compatibility using synthetic depth data")
 	// This allows a direct comparison of the RealSense colorizer and the hue encoder.
 	// Unfortunately, there are errors in the output of the RealSense hue encoder.
 	// Therefore this test does not actually test any values.
-	// Instead it shows value matches and mismatches 
+	// Instead it shows value matches and mismatches
 	// between the RealSense encoder ("RS") and this hue codec encoder ("HC")
 
-	auto depth_min_m = 0.0f;
-	auto depth_max_m = 1.531f;
 	float depth_scale = 0.001f;
-
-    // Initialize the RealSense-native colorizer to Hue colorization
-    rs2::colorizer color_map;
-	color_map.set_option(RS2_OPTION_HISTOGRAM_EQUALIZATION_ENABLED, 0);
-	color_map.set_option(RS2_OPTION_COLOR_SCHEME, 9.0f);		// Hue colorization
-	color_map.set_option(RS2_OPTION_MAX_DISTANCE, depth_max_m);
-	color_map.set_option(RS2_OPTION_MIN_DISTANCE, depth_min_m);
-
-	// Initialize the hue codec
-	HueCodec hc(depth_min_m, depth_max_m, depth_scale, false);
-
+	auto depth_min_m = 0.0f;
+	auto depth_max_m = (HUE_ENCODER_MAX + 1) * depth_scale;
 	int W = 40;	// (x)
 	int H = 40;	// (y)
-	int BPP = 2;
 
 	// Create Mat with values from 0 to 1600 (max encoding value is 1530)
 	cv::Mat depth = generate_synthetic_depth(W, H, 0, W*H, 1.0f);
 
-    rs2::software_device dev; // Create software-only device
-    auto depth_sensor = dev.add_sensor("Depth"); // Define a single depth sensor
+	// Use the RealSense SDK encoder to hue-encode the depth data
+	RSEncoder rs(H, W, depth_min_m, depth_max_m, depth_scale);
+	cv::Mat rs_color = rs.encode(depth);
 
-	rs2_intrinsics depth_intrinsics = { 
-		W, H, 
-		(float)W / 2, (float)H / 2, 
-		(float)W, (float)H,
-		RS2_DISTORTION_BROWN_CONRADY, { 0,0,0,0,0 } 
-	};
-
-    auto depth_stream = depth_sensor.add_video_stream({ 
-		RS2_STREAM_DEPTH, 0, 0,
-		W, H, 60, BPP,
-		RS2_FORMAT_Z16, depth_intrinsics 
-	});
-
-	// Compare all streams according to timestamp
-    dev.create_matcher( RS2_MATCHER_DEFAULT );  
-    rs2::syncer sync;
-    depth_sensor.open(depth_stream);
-    depth_sensor.start(sync);
-
-	int frame_number = 1;
-	rs2_time_t timestamp = (rs2_time_t)frame_number * 16;
-    auto domain = RS2_TIMESTAMP_DOMAIN_HARDWARE_CLOCK;
-
-	// Set synthetic depth frame data and parameters
-	depth_sensor.on_video_frame({ 
-		 (uint16_t*)depth.data,		// Frame pixels from capture API
-		 []( void * ) {},           // Custom deleter (if required)
-		 W * BPP,					// Stride
-		 BPP,
-		 timestamp, domain, frame_number,
-		 depth_stream, 
-		 0.001f						// depth unit
-	});  
-
-	rs2::frameset fset = sync.wait_for_frames();
-    rs2::frame rs_depth_frame = fset.first_or_default(RS2_STREAM_DEPTH);
-
-	// Use RealSense colorizer to hue-encode depth data
-	rs2::frame rs_color_frame = rs_depth_frame.apply_filter(color_map);
-
-	// Create OpenCV matrix from the colorized depth data
-	cv::Mat rs_color(H, W, CV_8UC3, (void*)rs_color_frame.get_data());
-
-	// Convert RealSense RGB colorized depth to OpenCV-standard BGR.
-	cv::cvtColor(rs_color, rs_color, cv::COLOR_RGB2BGR);
-
-	// Use hue encoder to hue-encode depth data
-	cv::Mat hc_color;
-	hc.encode(depth, hc_color);
+	// Use hue encoder to hue-encode the depth data
+	HueCodec hc(depth_min_m, depth_max_m, depth_scale, false); // Initialize the hue codec
+	cv::Mat hc_color = hc.encode(depth);
 
 	// Compare the two hue-encoded images
 	fmt::print("\nComparing RealSense (\"RS\") and hue codec (\"HC\") RGB values...\n");
@@ -113,7 +151,7 @@ TEST_CASE("test RealSense encoder compatibility using synthetic depth data")
 		{
 			auto d = depth.at<uint16_t>(i, j);
 
-			if (d < 1532)
+			if (d < HUE_ENCODER_MAX + 2)
 			{
 				auto rsc = rs_color.at<cv::Vec3b>(i, j);
 				auto hcc = hc_color.at<cv::Vec3b>(i, j);
@@ -134,5 +172,52 @@ TEST_CASE("test RealSense encoder compatibility using synthetic depth data")
 			}
 		}
 	}
+}
+
+
+TEST_CASE("compare psnr using the reference sequence")
+{
+	auto depth_min_m =  0.3f;
+	auto depth_max_m = 6.1f;
+	float depth_scale = 0.001f;
+
+	// Load the reference sequence
+	std::vector<cv::Mat> sequence;
+	load_reference_sequence(sequence);
+
+	RSEncoder rs(sequence.back().rows, sequence.back().cols, depth_min_m, depth_max_m, depth_scale);
+	HueCodec hc(depth_min_m, depth_max_m, depth_scale, false);
+
+	// Calculate the cumulative psnr for both encoders
+	float rs_psnr = 0.0f, hc_psnr = 0.0f;
+	for (auto i(sequence.begin()); i!=sequence.end(); ++i)
+	{
+		// Use the RealSense SDK encoder to hue-encode the depth data
+		cv::Mat rs_color = rs.encode(*i);
+
+		// Use hue encoder to hue-encode the depth data
+		cv::Mat hc_color = hc.encode(*i);
+
+		// Compare the PSNR of each.
+		cv::Mat rs_decoded = hc.decode(rs_color);
+		cv::Mat hc_decoded = hc.decode(hc_color);
+
+		// Uncomment below for frame-by-frame visual comparison
+		// imshow_depth("rs", rs_decoded);
+		// imshow_depth("hc", rs_decoded);
+		// cv::waitKey(0);
+
+		rs_psnr += psnr_depth(*i, rs_decoded, depth_max_m, depth_scale);
+		hc_psnr += psnr_depth(*i, hc_decoded, depth_max_m, depth_scale);
+	}
+
+	// Calculate the mean psnr
+	rs_psnr /= (float)sequence.size();
+	hc_psnr /= (float)sequence.size();
+
+	fmt::print("\nEncoder performance comparison on the reference sequence:\n\n");
+	fmt::print("RealSense   hue encoding and decoding mean PSNR:  {:>5.1f}\n", rs_psnr);
+	fmt::print("Hue Encoder hue encoding and decoding mean PSNR:  {:>5.1f}\n", hc_psnr);
+	fmt::print("\nHue Encoder mean PSNR improvement over RealSense: {:>5.1f}\n", hc_psnr-rs_psnr);
 }
 
